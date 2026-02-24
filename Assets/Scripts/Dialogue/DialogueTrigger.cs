9ca5b02ed329004e496221c9d0e925fa408acf63
ssjuuuul <113347678+ssjuuuul@users.noreply.github.com>
2026-02-24 09:44:49 +0100

    Mentor animation and dialogue added
    The mentor is now a prefab character and has a standard animation. I also added some of the dialogue options we can implement

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    
+    //swith between f and autotrigger dialogue
+    public enum TriggerType
+    {
+        KeyPress,
+        AutoTrigger
+    }
+
     [Header("Dialogue Settings:")]
     [Tooltip("The Dialogue that will play when interacting with npc")]
     [SerializeField] private DialogueObject dialogueToPlay;
     
     [Header("Interaction Setting")]
     [Tooltip("Key Press to interact with npc")]
+    [SerializeField] private TriggerType triggerType = TriggerType.KeyPress;
     [SerializeField] private KeyCode interactKey = KeyCode.F;
     
     private Dialogue dialogueManager;
 
     private bool isPlayerInRange = false;
     private bool isDialogueActive = false;
-    
-    
+    private bool hasAutoTriggered = false;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,7 +34,7 @@ public class DialogueTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isPlayerInRange && Input.GetKeyDown(interactKey))
+        if (triggerType == TriggerType.KeyPress && isPlayerInRange && Input.GetKeyDown(interactKey))
         {
             Debug.Log("F pressed, dialogue active: " + dialogueManager.IsDialogueActive());
             if (!dialogueManager.IsDialogueActive())
@@ -50,7 +58,18 @@ public class DialogueTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerInRange = true;
+
+            if (triggerType == TriggerType.AutoTrigger && !dialogueManager.IsDialogueActive()&& hasAutoTriggered == false)
+            {
+                StartConversation();
+                hasAutoTriggered = true;
+            }
         }
+
+        //if(triggerType == TriggerType.AutoTrigger && other.CompareTag("Player") && !dialogueManager.IsDialogueActive())
+        //{
+        //    StartConversation();
+        //}
     }
     
     private void OnTriggerExit2D(Collider2D other)
