30a49dcf6842c6b56ffbd5d759f34bb820bbdc8f
FireKnight76 <fireknight593@gmail.com>
2026-03-04 14:28:38 +0100

    Created the day/night cycle

---
+++ Assets/Scripts/Shader/SkyCycle.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SkyCycle : MonoBehaviour
+{
+    [SerializeField]
+    private Material skybox;
+    [SerializeField]
+    private float elapsedTime;
+    [SerializeField]
+    private float timeScale = 2.5f;
+    [SerializeField]
+    private float slowScale = 60;
+    private float slowedTime;
+
+    private static readonly int Rotation = Shader.PropertyToID("_Rotation");
+    private static readonly int Exposure = Shader.PropertyToID("_Exposure");
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        slowedTime += Time.deltaTime/slowScale;
+        skybox.SetFloat(Rotation, elapsedTime * timeScale);
+        skybox.SetFloat(Exposure, Mathf.Clamp(Mathf.Abs(Mathf.Cos(slowedTime))* (float)1.5, 0.15f, 1.5f));
+    }
+}
