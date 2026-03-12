91d65dcadd908132db968437eb7eb77ca679fd1a
Enoksen <269411@stud.inn.no>
2026-03-12 14:57:54 +0100

    Added More Assets And Fixed a pauseMenu with working buttons.
    Havent tested Resolution and fullscreen switching in Editor

---
+++ Assets/Scripts/Menu/PauseMenu/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    
+    public GameObject Container;
+    
+    public GameObject SettingsContainer;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        Container.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    
+    public void Pause()
+    {
+        Container.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void Settings()
+    {
+        Container.SetActive(false); 
+        SettingsContainer.SetActive(true);
+    }
+    public void Quit()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+    
+
+}
