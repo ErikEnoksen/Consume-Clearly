c9e9e5b705ba791c3998d541d37796d773cd6c90
Enoksen <erikenoksen@gmail.com>
2026-04-15 11:32:23 +0200

    Refatoring PlayModeTesting for new Codebase

--- Assets/Tests/PlayMode/PlayerManager_PlayModeTests.cs
+++ Assets/Tests/PlayMode/PlayerManager_PlayModeTests.cs
@@ -1,4 +1,3 @@
-// filepath: c:\Users\ianko\Consume-Clearly\Assets\Tests\PlayMode\PlayerManager_PlayModeTests.cs
 using System.Collections;
 using NUnit.Framework;
 using Player;
@@ -9,44 +8,48 @@ namespace Tests.PlayMode
 {
     public class PlayerManager_PlayModeTests
     {
+        private GameObject pmGo;
+        private PlayerManager pm;
+        private GameObject prefab;
+
+        [SetUp]
+        public void SetUp()
+        {
+            pmGo = new GameObject("PlayerManager");
+            pm = pmGo.AddComponent<PlayerManager>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (pmGo != null) Object.DestroyImmediate(pmGo);
+            if (prefab != null) Object.DestroyImmediate(prefab);
+            prefab = null;
+        }
+
         [UnityTest]
         public IEnumerator SpawnPlayer_NoPrefab_LogsErrorAndDoesNotCreate()
         {
-            var pmGo = new GameObject("PlayerManager");
-            var pm = pmGo.AddComponent<PlayerManager>();
-
-            // Ensure playerPrefab is null
-            // Capture logs
-            LogAssert.Expect(UnityEngine.LogType.Error, "Player Prefab is not assigned in PlayerManager!");
+            LogAssert.Expect(LogType.Error, "Player Prefab is not assigned in PlayerManager!");
 
             pm.SpawnPlayer(Vector3.zero);
             yield return null;
 
             Assert.IsNull(pm.GetPlayer());
-
-            Object.DestroyImmediate(pmGo);
         }
 
         [UnityTest]
         public IEnumerator SpawnPlayer_PrefabMissingGroundCheck_LogsErrorAfterInstantiate()
         {
-            var pmGo = new GameObject("PlayerManager2");
-            var pm = pmGo.AddComponent<PlayerManager>();
-
-            // create a prefab without GroundCheck
-            var prefab = new GameObject("PlayerPrefab");
-            // assign via reflection
-            typeof(PlayerManager).GetField("playerPrefab", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+            prefab = new GameObject("PlayerPrefab");
+            typeof(PlayerManager)
+                .GetField("playerPrefab", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
                 .SetValue(pm, prefab);
 
-            LogAssert.Expect(UnityEngine.LogType.Error, "Spawned player prefab is missing a GroundCheck object!");
+            LogAssert.Expect(LogType.Error, "Spawned player prefab is missing a GroundCheck object!");
 
             pm.SpawnPlayer(Vector3.zero);
             yield return null;
-
-            // Even though it may have been instantiated, it should have logged the error and left player as null or destroyed; just ensure the error was logged
-            Object.DestroyImmediate(pmGo);
-            Object.DestroyImmediate(prefab);
         }
     }
 }
