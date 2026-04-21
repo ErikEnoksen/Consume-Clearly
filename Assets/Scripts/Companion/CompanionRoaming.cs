d876d2d9c688bcb299aeeab06fbde843ded80e53
Enoksen <erikenoksen@gmail.com>
2026-04-21 12:07:40 +0200

    NPC Roaming with X chechpoints as Control Points and Uncheked Companion Collision

---
+++ Assets/Scripts/Companion/CompanionRoaming.cs
@@ -0,0 +1,99 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class CompanionRoaming : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    
+    [SerializeField] private float speed;
+    [SerializeField] private float leftPatrolX, rightPatrolX;
+    
+    [SerializeField] private float minPauseTime, maxPauseTime;
+    [SerializeField] private float minWalkTime, maxWalkTime;
+     
+    [SerializeField] private int facingDirection = 1;
+
+    private bool isFlipping;
+    private float randomTime, timer;
+    private bool isWalking = true;
+    private bool inDialogue = false;
+    
+    private void OnEnable()
+    {
+        Dialogue.OnDialogueStarted += HandleDialogueStarted;
+        Dialogue.OnDialogueEndedCompanion += HandleDialogueEnded;
+    }
+
+    private void OnDisable()
+    {
+        Dialogue.OnDialogueStarted -= HandleDialogueStarted;
+        Dialogue.OnDialogueEndedCompanion -= HandleDialogueEnded;
+    }
+
+    private void Start()
+    {
+        randomTime = Random.Range(minWalkTime, maxWalkTime);
+        animator.SetBool("isWalking", isWalking ? true : false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inDialogue) return;
+        
+        timer += Time.deltaTime;
+
+        if (timer >= randomTime)
+            StateChange();
+        
+        if (!isFlipping && (transform.position.x > rightPatrolX || transform.position.x < leftPatrolX))
+            StartCoroutine(Flip());
+        if (isWalking)
+        rb.linearVelocity = Vector2.right * facingDirection * speed;
+    }
+    
+    private void HandleDialogueStarted(CompanionFriendship companion)
+    {
+        if (companion != null && companion.transform.IsChildOf(transform))
+        //if (companion != null && companion.gameObject == gameObject)
+        {
+            inDialogue = true;
+            isWalking = false;
+            animator.SetBool("isWalking", false);
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+    private void HandleDialogueEnded(CompanionFriendship companion)
+    {
+        if (companion != null && companion.transform.IsChildOf(transform))
+        //if (companion != null && companion.gameObject == gameObject)
+        {
+            inDialogue = false;
+            isWalking = true;
+            animator.SetBool("isWalking", true);
+            timer = 0f;
+            randomTime = Random.Range(minWalkTime, maxWalkTime);
+        }
+    }
+    
+    IEnumerator Flip()
+    {
+        isFlipping = true;
+        transform.Rotate(0f, 180f, 0f);
+        facingDirection *= -1;
+        yield return new WaitForSeconds(0.7f);
+        isFlipping = false;
+    }
+    void StateChange()
+    {
+        isWalking = !isWalking;
+        animator.SetBool("isWalking", isWalking ? true : false);
+        randomTime = isWalking ? Random.Range(minWalkTime, maxWalkTime) : Random.Range(minPauseTime, maxPauseTime);
+        timer = 0f;
+    }
+}
